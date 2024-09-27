866eac4e312bad27e75e95ffd0e28493699e792c
Pablo Asensi <142694239+AsensiDev@users.noreply.github.com>
2024-09-27 19:07:51 +0200

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,62 @@
 import { useState } from "react"
+import confetti from "canvas-confetti"
 import Square from "./components/Square"
-
-const TURNS = {
-  X: 'x',
-  O: 'o'
-}
+import { TURNS, WINNER_COMBOS } from './constants'
 
 function App() {
 
   const [board, setBoard] = useState(Array(9).fill(null)) 
   const [turn, setTurn] = useState(TURNS.X)
+  // null es que no hay ganador, false es que hay empate
+  const [winner, setWinner] = useState(null)
+
+  const checkWinner = (boardToCheck) => {
+    // saber si hay alguna opcion ganadora
+    for (const combo of WINNER_COMBOS) {
+      const [a, b, c] = combo
+      if(
+        boardToCheck[a] &&
+        boardToCheck[a] === boardToCheck[b] &&
+        boardToCheck[a] ===  boardToCheck[c]
+      ) {
+        return boardToCheck[a]
+      }
+    }
+    // si no hay ganador
+    return null
+  }
+
+  const resetGame = () => {
+    setBoard(Array(9).fill(null))
+    setTurn(TURNS.X)
+    setWinner(null)
+  }
+
+  const checkEndGame = (newBoard) => {
+    return newBoard.every((square) => square !== null)
+  }
 
   const updateBoard = (index) => {
+    // no actualizamos si hay algo o hay ganador
+    if (board[index] || winner) return
+    
+    // actualizar tablero
     const newBoard = [...board]
     newBoard[index] = turn
     setBoard(newBoard)
 
+    // cambiar el turno
     const newTurn = turn === TURNS.X ? TURNS.O : TURNS.X
     setTurn(newTurn)
+
+    //revisar si hay ganador
+    const newWinner = checkWinner(newBoard)
+    if(newWinner) {
+      confetti()
+      setWinner(newWinner)
+    } else if(checkEndGame(newBoard)) {
+      setWinner(false) // empate
+    }
   }
 
   return (
@@ -31,7 +70,7 @@ function App() {
               index={index}
               updateBoard={updateBoard}
             >
-              
+             {board[index]} 
             </Square>
           )
         })}
@@ -45,6 +84,34 @@ function App() {
           {TURNS.O}
         </Square>
       </section>
+
+      <button onClick={resetGame}>
+        Reiniciar
+      </button>
+
+      {
+        winner !== null && (
+          <section className="winner">
+            <div className="text">
+              <h2>
+                {
+                  winner === false 
+                  ? 'Empate'
+                  : 'Ganó:'
+                }
+              </h2>
+
+              <header className="win">
+                {winner ? <Square>{winner}</Square> : <Square>🤝</Square>}
+              </header>
+
+              <footer>
+                <button onClick={resetGame}>Reiniciar</button>
+              </footer>
+            </div>
+          </section>
+        )
+      }
     </main>
   )
 }
